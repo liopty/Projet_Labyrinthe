ffdbf3c9010e07409e2d70128ae8b58d26f765f0
liopty <liopty@gmail.com>
2019-03-11 12:21:09 +0100

    test labyrinthe

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("test");
+        Map<Integer, Map<Integer, Case>> lesCases;
+        lesCases = new HashMap<>();
+        Case maCase = new Case(1,1,EnumCase.VIDE);
+        lesCases.put(1,new HashMap<Integer, Case>(1, maCase));
     }
 }
--- src/Labyrinthe.java
+++ src/Labyrinthe.java
@@ -1,2 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Labyrinthe {
+    Map<Integer, Map<Integer, Case>> lesCases;
+
+    public Labyrinthe() {
+        this.lesCases = new HashMap<>();
+    }
 }
