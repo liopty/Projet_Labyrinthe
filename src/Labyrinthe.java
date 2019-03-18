0a311b9d7120ef6623d9e32dd30bc251eea54a95
liopty <liopty@gmail.com>
2019-03-18 11:18:50 +0100

    Labyrinthe.java méthode creation laby à partir file path et méthode lecture laby dans console

--- src/Labyrinthe.java
+++ src/Labyrinthe.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,4 +8,74 @@ public class Labyrinthe {
     public Labyrinthe() {
         this.lesCases = new HashMap<>();
     }
+
+    private void addCase(Case c) {
+        if (!lesCases.containsKey(c.getX())){
+            lesCases.put(c.getX(), new HashMap<>());
+        }
+        lesCases.get(c.getX()).put(c.getY(), c);
+    }
+
+    public void init(String filePath){
+        try{
+            InputStream flux=new FileInputStream(filePath);
+            InputStreamReader lecture=new InputStreamReader(flux);
+            BufferedReader buff=new BufferedReader(lecture);
+            int value = 0;
+
+            int x = 0;
+            int y = 0;
+
+            while ((value = buff.read()) != -1){
+                char c = (char)value;
+
+                switch (c) {
+                    case '\n':
+                        y += 1;
+                        x = 0;
+                        break;
+                    case 'M':
+                        this.addCase(new Case(x,y, EnumCase.MUR));
+                       // System.out.println(c+" "+x+" "+y);
+                        x += 1;
+                        break;
+                    case 'E':
+                        this.addCase(new Case(x,y, EnumCase.ENTREE));
+                        //System.out.println(c+" "+x+" "+y);
+                        x += 1;
+                        break;
+                    case 'S':
+                        this.addCase(new Case(x,y, EnumCase.SORTIE));
+                        //System.out.println(c+" "+x+" "+y);
+                        x += 1;
+                        break;
+                    case '_':
+                        this.addCase(new Case(x,y, EnumCase.VIDE));
+                        //System.out.println(c+" "+x+" "+y);
+                        x += 1;
+                        break;
+                    default: System.out.println("ERR");
+                        break;
+                }
+
+            }
+            buff.close();
+        }
+        catch (Exception e){
+            System.out.println(e.toString());
+        }
+    }
+
+    public void lireLaby(){
+
+        for(Map.Entry<Integer, Map<Integer, Case>> entry : lesCases.entrySet()) {
+            int x = entry.getKey();
+            for (Map.Entry<Integer, Case> entry2: entry.getValue().entrySet()) {
+                int y = entry2.getKey();
+                System.out.println("x: "+x+" "+"y: "+y+" "+entry2.getValue().getType());
+            }
+
+        }
+    }
+
 }
